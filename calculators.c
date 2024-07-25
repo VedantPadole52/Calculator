58d7381e9e112de0ec665d5bec436adffe7df6db
Vedant Padole <vedant.officialwork0@gmail.com>
2024-07-25 20:11:30 +0530

    Update calculators.c

--- calculators.c
+++ calculators.c
@@ -2,31 +2,31 @@
 
 int main() {
     char operator;
-    int num1, num2, result;
+    double num1, num2, result;
 
     printf("Enter an operator (+, -, *, /): ");
     scanf("%c", &operator);
 
     printf("Enter two numbers: ");
-    scanf("%d,%d", &num1, &num2);
+    scanf("%lf,%lf", &num1, &num2);
 
     switch(operator) {
         case '+':
             result=num1 + num2;
-            printf("Result is %d",result);
+            printf("Result is %.2lf",result);
             break;
         case '-':
             result=num1 - num2;
-            printf("Result is %d", result);
+            printf("Result is %.2lf", result);
             break;
         case '*':
             result= num1*num2;
-            printf("Result is %d", result);
+            printf("Result is %.2lf", result);
             break;
         case '/':
             if (num2 != 0){
                 result=num1/num2;
-                printf("Result is %d",result);
+                printf("Result is %.2lf",result);
             }
             else{
                 printf("Error: Division by zero\n");
